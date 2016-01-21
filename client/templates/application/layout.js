4cf24fa4eb10e3d8bd328229528f5caac6b0c423
Robert Chen <rbchen16@gmail.com>
2016-01-21 14:59:43 -0500

    Optimized scrolling behavior
    -Scroll-induced clicks are now fired after scroll commands
    -User must hold an over scrolled page to fire a scroll-induced click

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -4,66 +4,69 @@ Template.layout.onRendered(function() {
 		scrolling past the bottom of the page
 	*/
 
-	//Only apply scroll-induced click after first the time the user scrolls to the
-	//very bottom or very top, otherwise will make this feature annoying to use since it 
-	//will be too responsive
 
-	downScrollCount = 0; 
-	upScrollCount = 0; 
+	(function() {        
+	    var timer;
+	    $(window).bind('scroll',function () {
+	        clearTimeout(timer);
+	        timer = setTimeout( refresh , 100 );
+	    });
+	    var refresh = function () { 
+	        afterScroll();
+	        console.log('Stopped Scrolling'); 
+    };
+	})();
 
-	window.onscroll = function() {
-		//window.scrollTo(0,+1);
+	var afterScroll = function() {
 		/*
 			Advances forward to the next page when scrolled to the bottom of the current page.
 			Also returns to the previous page when scrolled to the top of the current page.
 			No wrapping around occurs, but that can be easily changed by removing the 
 			specific caught cases
 		*/
-	    if ($(window).scrollTop() + $(window).height() > $(document).height() + 30) {
+	    if ($(window).scrollTop() + $(window).height() >= $(document).height() +20 ) {
 	    	//For when the user has scrolled to the bottom of the current page
-			downScrollCount += 1;
-	    	if (downScrollCount >= 2){
-		    	var nextPage = $('.active').next();
-		    	if ($('.active').is('.nav-block') === false){ //Scroll to page 1 from the 
-		    		//cover page
-		    		$('#b1').click();
-		    		downScrollCount = 0;
-		    	}else if (nextPage.is('#b1') === false){ //Do not wrap to first chapter upon reaching the end	        
-					if (flag === false){ //Ensures that only one scroll-induced click occurs at once     
-				        nextPage.click();
-				        downScrollCount = 0;
-				        //window.scrollTo(0,+1);
-				    }
-		    	}
+	    	var nextPage = $('.active').next();
+	    	if ($('.active').is('.nav-block') === false){ //Scroll to page 1 from the 
+	    		//cover page
+	    		$('#b1').click();
+	    	}else if (nextPage.is('#b1') === false){ //Do not wrap to first chapter upon reaching the end	        
+				if (flag === false){ //Ensures that only one scroll-induced click occurs at once     
+			        nextPage.click();
+			        reset();
+			    }
 	    	}
-	    }else if($(window).scrollTop() < -30){
+	    	
+	    }else if($(window).scrollTop() <= -20){
 	    	//For when the user has scrolled to the top of the current page. Technically,
 	    	//the user must scroll PAST the top of the page to trigger this function. This is
 	    	//in place so that this function does not automatically trigger when the user
 	    	//enters a few page (since they will be at the top of the page). The specific
 	    	//degree (-35) is related to the size of the background arrow indicating 
 	    	//scrollability
-	    	upScrollCount += 1;
-	    	if (upScrollCount >= 2){
-		    	var prevPage = $('.active').prev();
-		    	if ($('.active').is('#b1') === false){ //Do not wrap to last chapter upon reaching the beginning	        
-					if (flag === false){ //Ensures that only one scroll-induced click occurs at once     
-				        prevPage.click();
-				        upScrollCount = 0;
-				    }
-		    	}
+	    	var prevPage = $('.active').prev();
+	    	if ($('.active').is('#b1') === false){ //Do not wrap to last chapter upon reaching the beginning	        
+				if (flag === false){ //Ensures that only one scroll-induced click occurs at once     
+			        prevPage.click();
+			        reset();
+			    }
 	    	}
+	    	
 	    }
+	};
+
+	window.onscroll = function(){
 
 	    /*
 			Controls the appearance/disappearance of the background arrows, communicating
 			that scrolling past the page causes a jump
 	    */
+
 	    if ($(window).scrollTop() + $(window).height() > $(document).height() ){
 	    	//Make background down-arrow appear if valid
 	    	var nextPage=$('.active').next();
 	    	if (nextPage.is('#b1') === false) {//Do not show down-arrow on last page
-	    		$('#down-arrow').velocity({opacity:1},{duration: 1000});
+	    		$('#down-arrow').velocity({opacity:1},{duration: 500});
 	    		//Also move up the navbar so that it does not overlap with the down-arrow
 	    		var displace = $(window).scrollTop() + $(window).height() - $(document).height();
 	    		displace = displace + "px";
@@ -73,17 +76,21 @@ Template.layout.onRendered(function() {
 	    	//Make background up-arrow appear if valid
 	    	if ($('.active').is('#b1') === false && //Do not show up-arrow on first page
 	    			$('.active').is('.nav-block') === true) //or the cover page
-	    		$('#up-arrow').velocity({opacity:1},{duration: 1000});
+	    		$('#up-arrow').velocity({opacity:1},{duration: 500});
 	    }else{ 
-	    	//Reset background arrow css
-			$("#down-arrow").velocity("stop", true);
-			$("#up-arrow").velocity("stop", true);
-			$('nav').velocity("stop", true);
-	    	$('#down-arrow').css("opacity","0");
-	    	$('#up-arrow').css("opacity","0");
-	    	$('nav').css("bottom","0px");
+	    	reset();
 	    }
 	};
+
+	var reset = function(){
+    	//Reset background arrow css
+		$("#down-arrow").velocity("stop", true);
+		$("#up-arrow").velocity("stop", true);
+		$('nav').velocity("stop", true);
+    	$('#down-arrow').css("opacity","0");
+    	$('#up-arrow').css("opacity","0");
+    	$('nav').css("bottom","0px");
+	}
 
 	/*
 		Defines the functions involved in page-to-page transitions
